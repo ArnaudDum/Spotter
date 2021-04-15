a670d6a5e4b27f2e6a8c62192f1f391a0f28f520
Arnaud Dumoncel <71562306+ArnaudDum@users.noreply.github.com>
2021-04-15 21:34:22 +0200

    modif mongodb connect

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://DUM:Highjump210@spotter.ukh11.mongodb.net/SPOTTER?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+  .then(() => console.log('Connecté à MongoDB'))
+  .catch(() => console.log('Non connecté à MongoDB'));
+
+app.use(helmet());
+
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+//app.use('/api/post', postRoutes);
+//app.use('/api/auth', userRoutes);
 
 module.exports = app;
